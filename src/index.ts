33b48365a2b8e88b9fa27d922aee7b2691a758c8
FireIsGood <fireisadequate@gmail.com>
2025-03-10 20:48:55 -0700

    fix: port

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,7 @@ app.get("/", (c) => {
 serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port: 4512,
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
